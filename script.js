eecb474315da10e37bbdff9204e7fd4f748f9a5e
testpma <145681667+testpma@users.noreply.github.com>
2023-09-22 00:30:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-// script.js
-
-// Coordinates for Barcelona
-var barcelona = [41.3851, 2.1734];
-
-// Create a map centered on Barcelona
-var map = L.map('map').setView(barcelona, 13); // You can adjust the zoom level as needed
+// Create a map centered on Catalonia
+var map = L.map('map').setView([41.492, 1.560], 8);
 
 // Add a tile layer (you can choose a different provider if needed)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Create a marker for Barcelona
-L.marker(barcelona).addTo(map)
-    .bindPopup('Barcelona')
-    .openPopup();
+// Fetch the coordinates from "coordinates.json" using fetch
+fetch('coordinates.json')
+    .then(response => response.json())
+    .then(coordinates => {
+        console.log('Fetched coordinates:', coordinates); 
+		// Check if data is correctly fetched
+        // Create markers from the fetched coordinates
+        var markers = [];
+
+        coordinates.forEach(coordinate => {
+            var latitude = coordinate.latitude;
+            var longitude = coordinate.longitude;
+            markers.push(L.marker([latitude, longitude]).bindPopup('Marker ' + coordinate.id));
+        });
+
+        // Add markers to the map
+        markers.forEach(marker => {
+            marker.addTo(map);
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching coordinates:', error);
+    });
