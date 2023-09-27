5c7894a1882d3e512f7e04c2d4562668589e715b
testpma <145681667+testpma@users.noreply.github.com>
2023-09-27 23:31:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,96 @@
-// JavaScript code for your map functionality goes here
-// Initialize your map and add markers, layers, etc.
 
-// Example: Creating a leaflet map centered at Catalonia
-var map = L.map('map').setView([41.8719, 1.8349], 8);
-
+// Create the Leaflet map
+var map = L.map('map').setView([41.8719, 1.8349], 8); // Adjust the initial map view
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
 }).addTo(map);
 
-// Example: Show the filter popup when the button is clicked
-document.getElementById('btFilter').addEventListener('click', function () {
-    
-    // You can populate the filterPopup with your filter options here, e.g., from a JSON file.
+// Load data from markers.json
+fetch('markers.json')
+    .then((response) => response.json())
+    .then((data) => {
+        const listMu = document.getElementById('municipalityList');
+
+        // Iterate through the data and create checkboxes
+        data.forEach((item) => {
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.name = 'municipality';
+            checkbox.value = item.description; // Use the Description value
+
+            const label = document.createElement('label');
+            label.appendChild(checkbox);
+            label.appendChild(document.createTextNode(item.description));
+
+            listMu.appendChild(label);
+        });
+    });
+
+// JavaScript code for opening the popup in a new window
+document.getElementById('btMu').addEventListener('click', function () {
+	// Define the content for your popup window here
+	const popupContent = `
+		<html>
+		<head>
+			<!-- Your popup window head content here -->
+		</head>
+		<body>
+			<h3>Select Municipalities</h3>
+			<div id="listMu">
+				<!-- Checkbox options will be inserted here dynamically -->
+			</div>
+			<button id="applyFilter">Apply Filter</button>
+
+			<script>
+				fetch('markers.json')
+					.then((response) => response.json())
+					.then((data) => {
+						// Create a new window for the popup
+						const popupWindow = window.open('', '_blank', 'width=400,height=400');
+
+						// Create a container div for the list of checkboxes
+						const checkboxContainer = document.createElement('div');
+						checkboxContainer.style.overflowY = 'scroll';
+						checkboxContainer.style.maxHeight = '200px'; // Set the maximum height for scrollable content
+
+						// Iterate through the data and create checkboxes
+						data.forEach((item) => {
+							const checkbox = document.createElement('input');
+							checkbox.type = 'checkbox';
+							checkbox.name = 'municipality';
+							checkbox.value = item.description; // Use the Description value
+
+							const label = document.createElement('label');
+							label.appendChild(checkbox);
+							label.appendChild(document.createTextNode(item.description));
+
+							checkboxContainer.appendChild(label);
+						});
+
+						// Add the checkbox container to the popup window
+						popupWindow.document.body.appendChild(checkboxContainer);
+
+						// Create an "Apply Filter" button in the popup
+						const applyFilterButton = document.createElement('button');
+						applyFilterButton.id = 'applyFilter';
+						applyFilterButton.textContent = 'Apply Filter';
+						popupWindow.document.body.appendChild(applyFilterButton);
+
+						// Close the popup window when the "Apply Filter" button is clicked
+						applyFilterButton.addEventListener('click', function () {
+							popupWindow.close();
+						});
+
+						// Set the content type and close the popup window document
+						popupWindow.document.close();
+					});
+			</script>
+		</body>
+		</html>
+	`;
+
+	// Open a new popup window with your content
+	const popupWindow = window.open('', '_blank', 'width=400,height=400');
+	popupWindow.document.write(popupContent);
+	popupWindow.document.close();
 });
