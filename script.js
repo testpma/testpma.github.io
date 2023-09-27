c3b09a0fa498eefce9bd5ff2cb970d29b421eb38
testpma <145681667+testpma@users.noreply.github.com>
2023-09-27 23:40:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -26,71 +26,63 @@ fetch('markers.json')
         });
     });
 
-// JavaScript code for opening the popup in a new window
 document.getElementById('btMu').addEventListener('click', function () {
-	// Define the content for your popup window here
-	const popupContent = `
-		<html>
-		<head>
-			<!-- Your popup window head content here -->
-		</head>
-		<body>
-			<h3>Select Municipalities</h3>
-			<div id="listMu">
-				<!-- Checkbox options will be inserted here dynamically -->
-			</div>
-			<button id="applyFilter">Apply Filter</button>
+    // Open a new popup window with a specific size
+    const popupWindow = window.open('', '_blank', 'width=400,height=400');
 
-			<script>
-				fetch('markers.json')
-					.then((response) => response.json())
-					.then((data) => {
-						// Create a new window for the popup
-						const popupWindow = window.open('', '_blank', 'width=400,height=400');
+    // Define the content for your popup window here
+    const popupContent = `
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Filter: Municipalities</title>
+        </head>
+        <body>
+            <h3>Select Municipalities</h3>
+            <div id="listMu">
+                <!-- Checkbox options will be inserted here dynamically -->
+            </div>
+            <button id="applyFilter">Apply Filter</button>
 
-						// Create a container div for the list of checkboxes
-						const checkboxContainer = document.createElement('div');
-						checkboxContainer.style.overflowY = 'scroll';
-						checkboxContainer.style.maxHeight = '200px'; // Set the maximum height for scrollable content
+            <script>
+                // JavaScript code for the popup window
+                fetch('markers.json')
+                    .then((response) => response.json())
+                    .then((data) => {
+                        // Create a container div for the list of checkboxes
+                        const checkboxContainer = document.getElementById('listMu');
+                        checkboxContainer.style.overflowY = 'scroll';
+                        checkboxContainer.style.maxHeight = '200px'; // Set the maximum height for scrollable content
 
-						// Iterate through the data and create checkboxes
-						data.forEach((item) => {
-							const checkbox = document.createElement('input');
-							checkbox.type = 'checkbox';
-							checkbox.name = 'municipality';
-							checkbox.value = item.description; // Use the Description value
+                        // Iterate through the data and create checkboxes
+                        data.forEach((item) => {
+                            const checkbox = document.createElement('input');
+                            checkbox.type = 'checkbox';
+                            checkbox.name = 'municipality';
+                            checkbox.value = item.description; // Use the Description value
 
-							const label = document.createElement('label');
-							label.appendChild(checkbox);
-							label.appendChild(document.createTextNode(item.description));
+                            const label = document.createElement('label');
+                            label.appendChild(checkbox);
+                            label.appendChild(document.createTextNode(item.description));
 
-							checkboxContainer.appendChild(label);
-						});
+                            checkboxContainer.appendChild(label);
+                        });
 
-						// Add the checkbox container to the popup window
-						popupWindow.document.body.appendChild(checkboxContainer);
+                        // Create an "Apply Filter" button in the popup
+                        const applyFilterButton = document.getElementById('applyFilter');
 
-						// Create an "Apply Filter" button in the popup
-						const applyFilterButton = document.createElement('button');
-						applyFilterButton.id = 'applyFilter';
-						applyFilterButton.textContent = 'Apply Filter';
-						popupWindow.document.body.appendChild(applyFilterButton);
+                        // Close the popup window when the "Apply Filter" button is clicked
+                        applyFilterButton.addEventListener('click', function () {
+                            window.close();
+                        });
+                    });
+            </script>
+        </body>
+        </html>
+    `;
 
-						// Close the popup window when the "Apply Filter" button is clicked
-						applyFilterButton.addEventListener('click', function () {
-							popupWindow.close();
-						});
-
-						// Set the content type and close the popup window document
-						popupWindow.document.close();
-					});
-			</script>
-		</body>
-		</html>
-	`;
-
-	// Open a new popup window with your content
-	const popupWindow = window.open('', '_blank', 'width=400,height=400');
-	popupWindow.document.write(popupContent);
-	popupWindow.document.close();
+    // Write the popup content to the new window and close it after writing
+    popupWindow.document.write(popupContent);
+    popupWindow.document.close();
 });
+
