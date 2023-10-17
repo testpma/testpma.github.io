7ba8f4998f2f90b6bda54070773d14bb6864c83a
testpma <145681667+testpma@users.noreply.github.com>
2023-10-18 00:29:12 +0200

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,4 +3,16 @@ hidePanel();
 fetchData();
 initializeFilters();
 
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router'; // Import the router configuration
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router, // Attach the router to the Vue instance
+  render: h => h(App),
+}).$mount('#app');
+
+
 
